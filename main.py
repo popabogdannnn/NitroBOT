434d744d6bb9b7602bdb3c392ea6c39850cc3b6c
Bogdan-Ioan Popa <popa_bogdanioan@yahoo.com>
2024-03-17 23:23:30 +0200

    Bot-ul adauga userii in roluri inainte si dupa ce intra pe sv

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.utils import get
 from dotenv import load_dotenv
 import os
 import database
+import subprocess
 
 load_dotenv()
 
@@ -19,42 +20,72 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix = '$', intents = intents)
 
-async def add_info(guild, team_name):
-    role = get(guild.roles, name = team_name)
-    if(role is not None):
-        return
-    role = await guild.create_role(name = team_name, colour = discord.Colour(TEAM_ROLE_COLOR))
-    category = await guild.create_category(name = team_name)
-
-    overwrites = {
-        guild.default_role: discord.PermissionOverwrite(read_messages=False, connect=False),
-        role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True)
-    }
-
-    await category.edit(overwrites=overwrites)
-
-    await guild.create_text_channel(name = "text", category = category)
-    await guild.create_text_channel(name = "voice", category = category)
-
-async def update_server_info(ctx, db):    
+async def add_info(ctx, team_name):
     guild = ctx.guild
+
+    role = get(guild.roles, name = team_name)
+    if(role is None):
+        role = await guild.create_role(name = team_name, colour = discord.Colour(TEAM_ROLE_COLOR))
+        await ctx.send(f"Am creat rolul {team_name}.")
+    admin_role = get(guild.roles, name = "Admin")
+
+    
+    category = get(guild.categories, name = team_name)
+    if(category is None):
+        category = await guild.create_category(name = team_name)
+
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False, connect=False),
+            role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
+            admin_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True)
+        }
+
+        await category.edit(overwrites=overwrites)
+
+        await guild.create_text_channel(name = "text", category = category)
+        await guild.create_voice_channel(name = "voice", category = category)
+        await ctx.send(f"Am creat categoria {team_name}")
+
+async def assign_user(guild, discord_name, team_name):
+    user = get(guild.members, name = discord_name)
+    if(user is None):
+        return
+    role = get(guild.roles, name = team_name)
+    await user.add_roles(role)
+
+
+async def update_server_info(ctx, db): 
     for _, participant in db.iterrows():
         team_name = participant[TEAM_NAME].strip()
-        
-        await add_info(guild, team_name)
+        discord_name = participant[DISCORD_USER].strip()
 
-
+        await add_info(ctx, team_name)
+        await assign_user(ctx.guild, discord_name, team_name)
 
 
 @bot.command()
+@commands.has_any_role("Admin")
 async def update(ctx, url):
-    print(url)
-
+    # subprocess.run(
+    #     args = ["wget", "-O", "new_db.csv", url],
+    #     capture_output = True,
+    # )
+    
     await update_server_info(ctx, db)
+
+    await ctx.send(f"Totul s-a desfasurat cu succes!")
 
 @bot.event
 async def on_ready():
     pass
+
+@bot.event
+async def on_member_join(member):
+    for _, participant in db.iterrows():
+        if(participant[DISCORD_USER].strip() == member.name):
+            await assign_user(member.guild, member.name, participant[TEAM_NAME].strip())
+            break
+            
 
 
 
