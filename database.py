64e59ac8d8628d0aaabe1109e1fa3a5537354818
Bogdan-Ioan Popa <popa_bogdanioan@yahoo.com>
2024-03-29 21:44:04 +0200

    Modificari

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 import csv
 import pandas as pd
 
+DISCORD_USER = 'Username Discord'
+TEAM_NAME = 'Dacă da, care este numele echipei?'
+
 def load(path):
-    return pd.read_csv(path)
+    db = pd.read_csv(path)
+    db[DISCORD_USER] = db[DISCORD_USER].astype(str)
+    db[TEAM_NAME] = db[TEAM_NAME].astype(str)
+    return db
 
 def merge(db1, db2):
     return pd.concat([db1, db2], ignore_index=True).drop_duplicates()
--- main.py
+++ main.py
@@ -35,35 +35,47 @@ async def add_info(ctx, team_name):
         role = await guild.create_role(name = team_name, colour = discord.Colour(TEAM_ROLE_COLOR))
         await ctx.send(f"Am creat rolul {team_name}")
     admin_role = get(guild.roles, name = "Admin")
+    volunteer_role = get(guild.roles, name = "Voluntar")
+    mentor_role = get(guild.roles, name = "Mentor")
 
     team_name = team_name.lower()
+
     category = get(guild.categories, name = team_name)
+    
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False, connect=False),
+        role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
+        admin_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
+        mentor_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
+        volunteer_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
+    }
+
     if(category is None):
         category = await guild.create_category(name = team_name)
-
-        overwrites = {
-            guild.default_role: discord.PermissionOverwrite(read_messages=False, connect=False),
-            role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
-            admin_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True)
-        }
 
         await category.edit(overwrites=overwrites)
 
         await guild.create_text_channel(name = "text", category = category)
         await guild.create_voice_channel(name = "voice", category = category)
         await ctx.send(f"Am creat categoria {team_name}")
-
-async def assign_user(guild, discord_name, role_name):
+    else:
+        await category.edit(overwrites=overwrites)
+async def assign_user(guild, discord_name, role_name, old_role_name = None):
     user = get(guild.members, name = discord_name)
     if(user is None):
         return
-    role = get(guild.roles, name = role_name)
-    await user.add_roles(role)
+    if(old_role_name is not None):
+        old_role = get(guild.roles, name = old_role_name)
+        await user.remove_roles(old_role)
+    new_role = get(guild.roles, name = role_name)
+    await user.add_roles(new_role)
 
-
-async def update_server_info(ctx, db): 
+async def update_server_info(ctx, db, old_db): 
     for _, participant in db.iterrows():
         discord_name = participant[DISCORD_USER].strip()
+
+        await ctx.send(f"Verific participantul {discord_name}")
+
         if(participant[HAS_TEAM] == NO):
             team_name = SINGLE
         else:
@@ -73,9 +85,24 @@ async def update_server_info(ctx, db):
         if(participant[IS_ONSITE] == YES):
             location_role = ONSITE_ROLE
 
+        user_old_info = old_db.loc[old_db[DISCORD_USER] == discord_name]
+
+        old_team_name = None
+        old_location_role = None
+        for _, old_participant in user_old_info.iterrows():
+            if(old_participant[HAS_TEAM] == NO):
+                old_team_name = SINGLE
+            else:
+                old_team_name = participant[TEAM_NAME].strip()
+            
+            old_location_role = ONLINE_ROLE
+            if(participant[IS_ONSITE] == YES):
+                old_location_role = ONSITE_ROLE
+
+
         await add_info(ctx, team_name)
-        await assign_user(ctx.guild, discord_name, team_name)
-        await assign_user(ctx.guild, discord_name, location_role)
+        await assign_user(ctx.guild, discord_name, team_name, old_team_name)
+        await assign_user(ctx.guild, discord_name, location_role, old_location_role)
 
 
 @bot.command()
@@ -85,8 +112,8 @@ async def update(ctx):
     for att in ctx.message.attachments: 
         await att.save("new_db.csv")
         new_db = database.load("new_db.csv")
-        db = database.merge(db, new_db)
-        await update_server_info(ctx, db)
+        await update_server_info(ctx, new_db, db)
+        db = new_db
 
     database.save(db, "db.csv")
     await ctx.send(f"Totul s-a desfasurat cu succes!")
