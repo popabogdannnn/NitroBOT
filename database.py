e83c4ea48082ef02d8ca3dccb91058aba30066c2
Bogdan <popa_bogdanioan@yahoo.com>
2025-03-23 13:46:38 +0000

    Botul foloseste Sheet API

--- database.py
+++ database.py
@@ -1,18 +1,21 @@
 import csv
 import pandas as pd
+import gspread
 
 DISCORD_USER = 'Username Discord'
-TEAM_NAME = 'Dacă da, care este numele echipei?'
+TEAM_NAME = 'Daca da, care este numele echipei'
 
-def load(path):
-    db = pd.read_csv(path)
+gc = gspread.oauth()
+
+SPREADSHEET_ID = "16bMBw_XNP-tUQP5H1Zdd2C_oOjJTxnI7eNnXWvU0GqY"
+SHEET_ID = "1439690484"
+sheet = gc.open_by_key(SPREADSHEET_ID).get_worksheet_by_id(SHEET_ID)
+
+def load():
+    db = pd.DataFrame(sheet.get_all_records())
     db[DISCORD_USER] = db[DISCORD_USER].astype(str)
     db[TEAM_NAME] = db[TEAM_NAME].astype(str)
     return db
 
-def merge(db1, db2):
-    return pd.concat([db1, db2], ignore_index=True).drop_duplicates()
 
-def save(db, path):
-    db.to_csv(path, index=False)
     
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.utils import get
 from dotenv import load_dotenv
 import os
@@ -10,30 +10,32 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 DB_PATH = os.getenv('DB_PATH')
-TEAM_NAME = 'Dacă da, care este numele echipei?'
+TEAM_NAME = 'Daca da, care este numele echipei'
 DISCORD_USER = 'Username Discord'
-HAS_TEAM = 'Faci deja parte dintr-o echipă?'
+HAS_TEAM = 'Faci parte deja dintr-o echipă?'
 IS_ONSITE = 'Vei participa fizic la competiție?'
 TEAM_ROLE_COLOR = 0x2741F8
 YES = "Da"
 NO = "Nu"
+NO_PARTICIPATION = "Nu voi participa la hackathon"
 SINGLE = "Single"
-ONLINE_ROLE = "Online"
-ONSITE_ROLE = "On-site"
+
+GUILD_ID = 1348244782178107462
+GUILD = None
 
 intents = discord.Intents.all()
 
-db = database.load(DB_PATH)
+db = database.load()
 
 bot = commands.Bot(command_prefix = '$', intents = intents)
 
-async def add_info(ctx, team_name):
-    guild = ctx.guild
+async def add_info(team_name):
+    guild = GUILD
 
     role = get(guild.roles, name = team_name)
     if(role is None):
         role = await guild.create_role(name = team_name, colour = discord.Colour(TEAM_ROLE_COLOR))
-        await ctx.send(f"Am creat rolul {team_name}")
+
     admin_role = get(guild.roles, name = "Admin")
     volunteer_role = get(guild.roles, name = "Voluntar")
     mentor_role = get(guild.roles, name = "Mentor")
@@ -57,9 +59,9 @@ async def add_info(ctx, team_name):
 
         await guild.create_text_channel(name = "text", category = category)
         await guild.create_voice_channel(name = "voice", category = category)
-        await ctx.send(f"Am creat categoria {team_name}")
     else:
         await category.edit(overwrites=overwrites)
+
 async def assign_user(guild, discord_name, role_name, old_role_name = None):
     user = get(guild.members, name = discord_name)
     if(user is None):
@@ -70,76 +72,77 @@ async def assign_user(guild, discord_name, role_name, old_role_name = None):
     new_role = get(guild.roles, name = role_name)
     await user.add_roles(new_role)
 
-async def update_server_info(ctx, db, old_db): 
+async def update_server_info(db, old_db): 
+    global GUILD
     for _, participant in db.iterrows():
+        if(participant[HAS_TEAM] == NO_PARTICIPATION):
+            continue
         discord_name = participant[DISCORD_USER].strip()
-
-        await ctx.send(f"Verific participantul {discord_name}")
 
         if(participant[HAS_TEAM] == NO):
             team_name = SINGLE
         else:
             team_name = participant[TEAM_NAME].strip()
-        
-        location_role = ONLINE_ROLE
-        if(participant[IS_ONSITE] == YES):
-            location_role = ONSITE_ROLE
 
         user_old_info = old_db.loc[old_db[DISCORD_USER] == discord_name]
 
         old_team_name = None
-        old_location_role = None
+        
+
         for _, old_participant in user_old_info.iterrows():
             if(old_participant[HAS_TEAM] == NO):
                 old_team_name = SINGLE
             else:
                 old_team_name = participant[TEAM_NAME].strip()
             
-            old_location_role = ONLINE_ROLE
-            if(participant[IS_ONSITE] == YES):
-                old_location_role = ONSITE_ROLE
+
+        
+        print(f"Discord {discord_name}, team: {team_name}, old_team: {old_team_name}")
+        await add_info(team_name)
+        await assign_user(GUILD, discord_name, team_name, old_team_name)
 
 
-        await add_info(ctx, team_name)
-        await assign_user(ctx.guild, discord_name, team_name, old_team_name)
-        await assign_user(ctx.guild, discord_name, location_role, old_location_role)
-
-
-@bot.command()
-@commands.has_any_role("Admin")
-async def update(ctx):
+@tasks.loop(seconds = 300)
+async def loop_update():
     global db
-    for att in ctx.message.attachments: 
-        await att.save("new_db.csv")
-        new_db = database.load("new_db.csv")
-        await update_server_info(ctx, new_db, db)
-        db = new_db
-
-    database.save(db, "db.csv")
-    await ctx.send(f"Totul s-a desfasurat cu succes!")
+    new_db = database.load()
+    if(new_db.equals(db)):
+        return
+    await update_server_info(new_db, db)
+    db = new_db
 
 @bot.event
 async def on_ready():
-    pass
+    global GUILD, GUILD_ID
+    for guild in bot.guilds:
+        if(guild.id == GUILD_ID):
+            GUILD = guild
+    if not loop_update.is_running():
+        loop_update.start()
 
 @bot.event
 async def on_member_join(member):
     global db
+    if(member.guild.id != GUILD_ID):
+        return
     for _, participant in db.iterrows():
         if(participant[DISCORD_USER].strip() == member.name):
             if(participant[HAS_TEAM] == NO):
                 team_name = SINGLE
             else:
                 team_name = participant[TEAM_NAME].strip()
-
-            location_role = ONLINE_ROLE
-            if(participant[IS_ONSITE] == YES):
-                location_role = ONSITE_ROLE
-            
-            await assign_user(member.guild, member.name, team_name)
-            await assign_user(member.guild, member.name, location_role)
+            await assign_user(GUILD, team_name)
             break
-            
+
+@bot.command()
+async def update(ctx):
+    global db
+    await ctx.send(f"Adaug echipe...")
+    new_db = database.load()
+    await update_server_info(new_db, db)
+    db = new_db
+    await ctx.send(f"Totul s-a desfasurat cu succes!")
+
 bot.run(TOKEN)
 
 
